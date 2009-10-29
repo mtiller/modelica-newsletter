0b88c0f46c374ace74323d00681643738176e680
Dietmar Winkler <Dietmar.Winkler@hit.no>
2009-10-29 08:19:29 +0100

    Changes of Mike (done on the server) applied and uploaded.

--- modelica/newsletter/views.py
+++ modelica/newsletter/views.py
@@ -5,12 +5,16 @@ from modelica.newsletter.models import *
 def index(request):
     return HttpResponse("Newsletter index")
 
+def _dweight(s1,s2):
+    return s1['obj'].weight-s2['obj'].weight;
+
 def render(request, id):
     newsletter = Newsletter.objects.get(id=id)
     secobjs = Section.objects.filter(issue=newsletter)
     secs = []
     for obj in secobjs:
         secs.append({'obj': obj, 'items': Item.objects.filter(section=obj)})
+    secs.sort(_dweight)
     t = loader.get_template('render.html')
     c = Context({'issue': newsletter, 'secs': secs})
     return HttpResponse(t.render(c))
